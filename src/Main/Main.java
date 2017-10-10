43a9b9afaa29d1e90350db65eed5d6a880b08983
Jesus Sanchez <Jesusanchez40@gmail.com>
2017-10-10 14:51:55 -0500

    first commit

---
+++ src/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class Main
+{
+    public final static int portNumber = 3080;
+    public static String fileToSend ="";
+
+    public static void main(String [] args)
+    {
+        System.out.println("Select file to send over to Juan");
+
+    }
+
+}
